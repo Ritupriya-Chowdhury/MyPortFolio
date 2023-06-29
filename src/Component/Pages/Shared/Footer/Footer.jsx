7f896788bd7321f4be5fa5564da039ed42bc2679
Ritupriya <ritupriyabgc@gmail.com>
2023-06-29 07:52:20 +0600

    footer

--- src/Component/Pages/Shared/Footer/Footer.jsx
+++ src/Component/Pages/Shared/Footer/Footer.jsx
@@ -1,9 +1,53 @@
-
+import { Link } from "react-router-dom";
+import {AiFillHome,AiFillLinkedin,AiFillGithub } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="mt-16">
+            <footer className="footer footer-center p-10 bg-blue-950 text-primary-content">
+                <div>
+
+                    <p className="font-bold text-3xl">
+                        RituPriya
+                    </p>
+                    <div className="text-xl md:flex">
+                       <p className="mr-4">
+                        <Link to='/'><p className="mt-1"><AiFillHome></AiFillHome></p></Link>
+                       </p>
+                       <p className="mr-4">
+                        <Link to='/aboutMe'>About Me</Link>
+                       </p>
+                       <p className="mr-4">
+                        <Link to='/resume'>Resume</Link>
+                       </p>
+                       <p className="mr-4">
+                        <Link to='/projects'>Projects</Link>
+                       </p>
+                       <p className="mr-4">
+                        <Link to='/contact'>Contact</Link>
+                       </p>
+                        
+                    </div>
+                    <p>Copyright © 2023 - All right reserved</p>
+                </div>
+                <div>
+                    <div className="grid grid-flow-col gap-4 text-3xl">
+                        <p>
+                            <Link to='https://web.facebook.com/ritupriya.chowdhury.5'><BsFacebook/></Link>
+                        </p>
+                       
+                        <p>
+                            <Link to='https://www.linkedin.com/in/ritupriya-chowdhury-7019a1251/'><AiFillLinkedin/></Link>
+                        </p>
+                       
+                        <p>
+                            <Link to='https://github.com/Ritupriya-Chowdhury'><AiFillGithub/></Link>
+                        </p>
+                       
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 };
--- src/Component/Pages/Shared/Header/Header.jsx
+++ src/Component/Pages/Shared/Header/Header.jsx
@@ -1,38 +1,38 @@
 import { Link } from "react-router-dom";
-
+import { AiFillHome } from "react-icons/ai";
 
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-transparent">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-blue-800 text-xl font-bold">
-      <li><Link>Home</Link></li>
-      <li tabIndex={0}>
-        <Link>Resume</Link>
-      </li>
-      <li><Link>Projects</Link></li>
-      </ul>
-    </div>
-    <a className="italic font-bold text-2xl"><span className="text-blue-700">M</span><span className="text-red-600">y</span> <span className="text-orange-700">P</span><span className="text-blue-700">or</span><span className="text-orange-700">t</span><span className="text-red-600">fo</span><span className="text-blue-700">li</span><span className="text-orange-700">o</span></a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1 text-blue-800 text-xl font-bold">
-      <li><Link>Home</Link></li>
-      <li tabIndex={0}>
-        <Link>Resume</Link>
-      </li>
-      <li><Link>Projects</Link></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn btn-primary btn-outline">Download Resume</a>
-  </div>
-</div>
+            <div className="navbar bg-transparent fixed">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-primary btn-outline lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-blue-800 text-xl font-bold">
+                            <li><Link to='/'><AiFillHome/></Link></li>
+                            <li tabIndex={0}>
+                                <Link to='/resume'>Resume</Link>
+                            </li>
+                            <li><Link to='/projects'>Projects</Link></li>
+                        </ul>
+                    </div>
+                    <p className="italic font-bold text-2xl ml-4"><span className="text-blue-700">M</span><span className="text-red-600">y</span> <span className="text-orange-700">P</span><span className="text-blue-700">or</span><span className="text-orange-700">t</span><span className="text-red-600">fo</span><span className="text-blue-700">li</span><span className="text-orange-700">o</span></p>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 text-blue-800 text-xl font-bold">
+                        <li><Link to='/'><p className="mt-1"><AiFillHome/></p></Link></li>
+                        <li tabIndex={0}>
+                            <Link to='/resume'>Resume</Link>
+                        </li>
+                        <li><Link to='/projects'>Projects</Link></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn btn-primary btn-outline">Download Resume</a>
+                </div>
+            </div>
         </div>
     );
 };
